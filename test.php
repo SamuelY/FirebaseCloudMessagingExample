b7ef71b034582cf9241d431de8fa519953d3f988
Samuel <shun.miao@gmail.com>
2019-07-02 18:02:47 +0800

    Create test.php
    example for cloud messaging

---
+++ test.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Google_Client;
+use Google_Service_FirebaseCloudMessaging;
+
+
+
+class TestController extends Controller
+{
+    function googleApi() {
+        $file_path = Storage::disk('local')->getDriver()->getAdapter()->getPathPrefix().'credentials.json';
+        putenv('GOOGLE_APPLICATION_CREDENTIALS='.$file_path);
+
+        $client = new Google_Client();
+        $client->useApplicationDefaultCredentials();
+        $client->addScope(Google_Service_FirebaseCloudMessaging::CLOUD_PLATFORM);
+
+        $httpClient = $client->authorize();
+
+        // Your Firebase project ID
+        $project = "telehealth-esp";
+        // Creates a notification for subscribers to the debug topic
+        $message = [
+            "message" => [
+                "token" => "token here",
+                "notification" => [
+                    "title" => "FCM Message",
+                    "body" => "This is an FCM notification message!",
+                ]
+            ]
+        ];
+        // Send the Push Notification - use $response to inspect success or errors
+        $response = $httpClient->post("https://fcm.googleapis.com/v1/projects/{$project}/messages:send", ['json' => $message]);
+        $return = json_decode($response->getBody()->getContents());
+        return response()->json($return);
+
+    }
+}
